d2bb3da904229c706d2182215c5d931ed15bfdf2
TrentonPottruff <jayitinc@gmail.com>
2014-01-21 22:18:53 -0500

    dsd

--- Jay Craft/Jay Craft/Game1.cs
+++ Jay Craft/Jay Craft/Game1.cs
@@ -15,6 +15,12 @@ namespace Jay_Craft
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        VertexBuffer vertexBuffer;
+        BasicEffect basicEffect;
+        Matrix world;
+        Matrix view;
+        Matrix projection;
+        float aspectRatio;
 
         public Game1()
         {
@@ -30,11 +36,21 @@ namespace Jay_Craft
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            basicEffect = new BasicEffect(GraphicsDevice);
+
+            VertexPositionColor[] verticies = new VertexPositionColor[3];
+            verticies[0] = new VertexPositionColor(new Vector3(-.5f, .5f, 0), Color.Red);
+            verticies[1] = new VertexPositionColor(new Vector3(.5f, .5f, 0), Color.Red);
+            verticies[2] = new VertexPositionColor(new Vector3(-.5f, -.5f, 0), Color.Red);
+
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(verticies);
         }
 
         protected override void UnloadContent()
         {
-
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -42,12 +58,34 @@ namespace Jay_Craft
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
+            world = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+            view = Matrix.CreateLookAt(new Vector3(0, 0, -10), Vector3.Zero, Vector3.Up);
+            aspectRatio = graphics.PreferredBackBufferWidth / graphics.PreferredBackBufferHeight;
+            projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(30), aspectRatio, 0.01f, 100f);
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            basicEffect.World = world;
+            basicEffect.View = view;
+            basicEffect.Projection = projection;
+            basicEffect.VertexColorEnabled = true;
+
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+            }
 
             base.Draw(gameTime);
         }
